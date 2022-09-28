8d0c9c21731c68859a1f1e1f8b47fa33e563a602
Nepolean23 <nlean1996@gmail.com>
2022-09-28 19:53:55 +0530

    fliping coins in loop

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -2,11 +2,14 @@
 
 echo " Flipping a coin to display heads or tail";
 
-a=$((RANDOM%2));
+for ((i=0; i<=5; i++))
+do
+	a=$((RANDOM%2));
 
-if [ $a -eq 1 ]
-then
-	echo " It is head";
-else
-	echo "It is tail";
-fi
+	if [ $a -eq 1 ]
+	then
+		echo "It is head";	
+	else
+		echo "It is tail";
+	fi
+done
