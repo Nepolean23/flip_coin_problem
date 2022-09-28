02ea4b582e4a389553f82d14f0edb89687f20c59
Nepolean23 <nlean1996@gmail.com>
2022-09-28 21:02:17 +0530

    modified on showing no.of heads and tail

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -2,14 +2,21 @@
 
 echo " Flipping a coin to display heads or tail";
 
-for ((i=0; i<=5; i++))
-do
-	a=$((RANDOM%2));
+H=0;
+T=1;
 
-	if [ $a -eq 1 ]
+	for ((i=0; i<=21; i++))
+	do
+		a=$((RANDOM%2));
+
+	if [ $a -eq $H ]
 	then
-		echo "It is head";	
-	else
-		echo "It is tail";
+		echo "It is head ";	
+	
+	elif [ $a -eq $T ]
+	then
+	
+		echo "It is tail ";
 	fi
+
 done
